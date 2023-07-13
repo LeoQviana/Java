dc7eb55c87eb960b68d172f3492a317100fe5a3d
LeoQviana <129958382+LeoQviana@users.noreply.github.com>
2023-07-13 15:57:46 -0300

    Add files via upload

---
+++ Encapsulamento/Numeros.java
@@ -0,0 +1,18 @@
+package Encapsulamento;
+
+public class Numeros {
+
+	private int valor;
+	
+	public int retorno() {
+		return valor;
+		
+	}
+		
+	public void modificar(int x) {
+		this.valor = x;
+	}
+
+	}
+
+
---
+++ Encapsulamento/UsoNumeros.java
@@ -0,0 +1,12 @@
+package Encapsulamento;
+
+public class UsoNumeros {
+
+	public static void main(String[] args) {
+		Numeros n1 = new Numeros();
+		n1.modificar(10);
+		System.out.println(n1.retorno());
+
+	}
+
+}
